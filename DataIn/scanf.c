5ac157a783b66366ee15aa4afd04c93f36139322
Lucas Gabriel <lucmsilva651@gmail.com>
2024-10-07 18:09:50 -0300

    datain with scanf

---
+++ DataIn/scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int age = 0;
+	
+	printf("Initial age: %d.\n", age);
+	
+	printf("Type your age:\n");
+	scanf("%d", &age);
+	
+	printf("Your age: %d.\n", age);
+}

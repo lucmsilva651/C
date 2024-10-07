01364201c6ef3087c12acbd91c4bdb5b2450fde2
Lucas Gabriel <lucmsilva651@gmail.com>
2024-10-07 17:52:59 -0300

    dataout

---
+++ DataOut/dataout.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	printf("Num: 6\n");
+	printf("Int: %d\n", 10);
+	printf("Real: %f.\n", 3.14159265);
+	printf("2char Real: %.2f.\n", 3.14159265);
+	printf("Text data: %c.\n", 'a');
+	printf("Text data: %s.\n", "testing");
+}

17f5fc14b700ec3cd2b40f60a3cf75948c977eac
Lucas Gabriel <lucmsilva651@gmail.com>
2024-10-07 17:34:50 -0300

    debug + removed .exe from .gitignore

---
+++ Debug/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Msg1\n");
+	printf("Msg2\n");
+	printf("Msg3\n");
+	printf("Msg4\n");
+	printf("Msg5\n");
+	return 0;
+}
